d105c7885c4d2937b25184baf6fc8e7f24cea236
songmoro <wlqgkr@naver.com>
2025-09-23 17:53:46 +0900

    Initial Commit

---
+++ Cerchio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cerchio
+//
+//  Created by 송재훈 on 9/23/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CerchioTests/CerchioTests.swift
@@ -0,0 +1,17 @@
+//
+//  CerchioTests.swift
+//  CerchioTests
+//
+//  Created by 송재훈 on 9/23/25.
+//
+
+import Testing
+@testable import Cerchio
+
+struct CerchioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
