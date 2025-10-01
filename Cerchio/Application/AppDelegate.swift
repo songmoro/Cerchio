5aababc6686311756b725b3805543b22345e35b7
songmoro <wlqgkr@naver.com>
2025-10-01 12:54:37 +0900

    마이그레이션

--- Cerchio/Application/AppDelegate.swift
+++ Cerchio/Application/AppDelegate.swift
@@ -6,11 +6,17 @@
 //
 
 import UIKit
+import RealmSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        configureRealm()
         return true
+    }
+
+    private func configureRealm() {
+        
     }
     
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
